5bf91239310733de289741112403c3887c62788c
anatolii <melnychuk.official@gmail.com>
2018-12-01 14:14:38 +0200

    fix test file Handlebsrs

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ console.log('1');
 
     Handlebars.registerPartial('file', Handlebars.templates.file);
     console.log(2);
-    let html = Handlebars.templates.content({ data });
+    let html = Handlebars.templates.content({ person: data});
 
     document.getElementById('main').innerHTML = html;
 
