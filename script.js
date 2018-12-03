9a1ec2f8f2ec37845b964a83c4ba6c5989df7d2e
anatolii <melnychuk.official@gmail.com>
2018-12-03 17:12:59 +0200

    now work

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 (function f(window, Handlebars) {
-    let data = {
-        people: [
+    let data = [
             {
                 name: 'Anatolii',
                 surname: 'Melnychuk',
@@ -16,12 +15,11 @@
                 surname: 'Litvin',
                 age: 33
             }
-        ]
-    };
+        ];
 
-    Handlebars.registerPartial('file', Handlebars.templates.file);
+    Handlebars.registerPartial('file', Handlebars.templates['file.hbs']);
 
-    let html = Handlebars.templates.content({ person: data});
+    let html = Handlebars.templates['content.hbs']({ person: data});
 
     document.getElementById('main').innerHTML = html;
 
